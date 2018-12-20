b5763697a3d5cf458d32a89d4f023e2df6928172
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:39:50 +0100

    rust: Reformat examples.

--- software/examples/rust/example_simple_button.rs
+++ software/examples/rust/example_simple_button.rs
@@ -1,6 +1,8 @@
-use std::{error::Error, io};
+use std::{io, error::Error};
 
-use tinkerforge::{ip_connection::IpConnection, rgb_led_button_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, 
+                  rgb_led_button_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -11,16 +13,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let rlb = RgbLedButtonBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Get current button state.
-    let state = rlb.get_button_state().recv()?;
+		// Get current button state.
+let state = rlb.get_button_state().recv()?;
 
-    if state == RGB_LED_BUTTON_BRICKLET_BUTTON_STATE_PRESSED {
-        println!("State: Pressed");
-    } else if state == RGB_LED_BUTTON_BRICKLET_BUTTON_STATE_RELEASED {
-        println!("State: Released");
-    }
+		if state == RGB_LED_BUTTON_BRICKLET_BUTTON_STATE_PRESSED {
+			println!("State: Pressed");
+		}
+		else if state == RGB_LED_BUTTON_BRICKLET_BUTTON_STATE_RELEASED {
+			println!("State: Released");
+		}
 
     println!("Press enter to exit.");
     let mut _input = String::new();
