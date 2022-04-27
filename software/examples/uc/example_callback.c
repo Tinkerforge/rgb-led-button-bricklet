7f5f830b83a1ea5dbc9c02be3085ed9c78b00dab
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:23:57 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_callback.c
+++ software/examples/uc/example_callback.c
@@ -1,17 +1,13 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_rgb_led_button.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_rgb_led_button.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your RGB LED Button Bricklet
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 // Callback function for button state changed callback
 static void button_state_changed_handler(TF_RGBLEDButton *device, uint8_t state,
@@ -29,7 +25,7 @@ static TF_RGBLEDButton rlb;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_rgb_led_button_create(&rlb, UID, hal), "create device object");
+	check(tf_rgb_led_button_create(&rlb, NULL, hal), "create device object");
 
 	// Register button state changed callback to function button_state_changed_handler
 	tf_rgb_led_button_register_button_state_changed_callback(&rlb,
--- software/examples/uc/example_simple_color.c
+++ software/examples/uc/example_simple_color.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_rgb_led_button.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_rgb_led_button.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your RGB LED Button Bricklet
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_RGBLEDButton rlb;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_rgb_led_button_create(&rlb, UID, hal), "create device object");
+	check(tf_rgb_led_button_create(&rlb, NULL, hal), "create device object");
 
 	// Set light blue color
 	check(tf_rgb_led_button_set_color(&rlb, 0, 170, 234), "call set_color");
